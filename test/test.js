0b6fb0f4cff62f3898a18755f41b7dc9dc913c82
Jas <jassaini@Jass-MacBook.local>
2017-05-12 13:05:42 -0400

    Commit

--- test/test.js
+++ test/test.js
@@ -12,23 +12,58 @@ var chaiHttp = require('chai-http');
 var expect = chai.expect;
 
 chai.use(chaiHttp);
+var should = chai.should();
 
-//chai.should();
+var timeOut = 15000;
 
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-driver.get('http://jaspal-webapp.herokuapp.com/');
-driver.get('http://jaspal-webapp.herokuapp.com/');
-
 test.describe('Authentication',function()
 {
+	this.timeout(timeOut);
+	
 	test.it('Sign Up', function()
 	{
+		driver.get('http://jaspal-webapp.herokuapp.com');
+		driver.wait(until.titleIs('Express'), 10000);
+		driver.get('http://jaspal-webapp.herokuapp.com');
+
 		driver.findElement(webdriver.By.id('signupEmail')).sendKeys('test@test.com');
-		driver.findElement(webdriver.By.id('signupPassword')).sendKeys('test@test.com');
+		driver.findElement(webdriver.By.id('signupPassword')).sendKeys('test');
 		driver.findElement(webdriver.By.id('signupButton')).click();
 	});
+
+	test.it('Log In', function()
+	{
+		driver.manage().deleteAllCookies();
+
+		driver.findElement(webdriver.By.id('loginEmail')).sendKeys('test@test.com');
+		driver.findElement(webdriver.By.id('loginPassword')).sendKeys('test');
+		driver.findElement(webdriver.By.id('loginButton')).click();
+	});
 })
+
+test.describe('Features', function()
+{
+	it('Post Expressions(Tweets)', function()
+	{
+
+	});
+
+	it('Follow Users', function()
+	{
+
+	});
+
+});
+
+test.describe('Log Out', function()
+{
+	it('Log Out', function()
+	{
+		driver.findElement(webdriver.By.id('logOut')).click();
+	});
+});
 
 
 
